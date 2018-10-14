f76cb7a7ea02a8f493e4bedc358eb2356f9e5680
LilyKuhn <lvk8738@rit.edu>
2018-10-14 17:49:54 -0400

    working on Row iterator

--- src/main/java/com/webcheckers/model/Row.java
+++ src/main/java/com/webcheckers/model/Row.java
@@ -1,6 +1,9 @@
 package com.webcheckers.model;
 
-public class Row /*implements Iterable<Space>*/{
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Row implements Iterable<Space>{
     private Space[] row;
     private int index;
 
@@ -14,5 +17,35 @@ public class Row /*implements Iterable<Space>*/{
 
     public int getIndex() {
         return index;
+    }
+
+    @Override
+    public Iterator<Row> iterator() {
+        return new RowIterator();
+    }
+
+    private class RowIterator implements Iterator<Row> {
+        private int cursor;
+
+        public RowIterator() {
+            this.cursor = 0;
+        }
+
+        public boolean hasNext() {
+            return this.cursor < 8;
+        }
+
+        public Integer next() {
+            if(this.hasNext()) {
+                int current = cursor;
+                cursor ++;
+                return current;
+            }
+            throw new NoSuchElementException();
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
     }
 }
