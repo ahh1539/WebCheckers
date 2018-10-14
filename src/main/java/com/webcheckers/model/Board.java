6ec40bbf4735ddee9792309ed67af136a2046cf1
Elijah Parrish <elijah.parrish321@gmail.com>
2018-10-14 16:47:37 -0400

    Fleshing out board

--- src/main/java/com/webcheckers/model/Board.java
+++ src/main/java/com/webcheckers/model/Board.java
@@ -4,11 +4,28 @@ public class Board {
 
     private final int LENGTH = 5;
     private final int WIDTH = 5;
-    private int[][] gameBoard; //2D array to store the game board
-
-    public Board(){
+    private int gameBoard[][] = new int [LENGTH][WIDTH];
 
 
-
+    public Board() {
+        for (int i = 0; i < LENGTH; i++)
+            for (int j = 0; j < WIDTH; j++)
+            {
+                gameBoard[i][j] = 0; //TODO initialized array to dummy values
+            }
     }
+
+
+    public int[][] updateBoard() {
+        //TODO update the board from the ui tier
+    }
+
+    public int getOccupied(int length, int width)
+    {
+        return gameBoard[length][width];
+    }
+
+
+
+
 }
