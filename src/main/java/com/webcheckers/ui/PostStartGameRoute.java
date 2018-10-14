e85d74268b7fc0f710fc30fdab133b87e35a7e62
Daria Chaplin <dxc4643@g.rit.edu>
2018-10-14 14:37:14 -0400

    Added a class for PostStartGameRoute

---
+++ src/main/java/com/webcheckers/ui/PostStartGameRoute.java
@@ -0,0 +1,14 @@
+package com.webcheckers.ui;
+
+import spark.*;
+
+public class PostStartGameRoute implements Route {
+
+    public PostStartGameRoute() {
+
+    }
+
+    public Object handle(Request request, Response response) {
+        return null;
+    }
+}
