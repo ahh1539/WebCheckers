bc86ea113a9e5a14d60489d8de6a45da88b160d1
Elijah Parrish <elijah.parrish321@gmail.com>
2018-10-14 16:12:01 -0400

    Added some attributes to Board

--- src/main/java/com/webcheckers/model/Board.java
+++ src/main/java/com/webcheckers/model/Board.java
@@ -2,7 +2,13 @@ package com.webcheckers.model;
 
 public class Board {
 
+    private final int LENGTH = 5;
+    private final int WIDTH = 5;
+    private int[][] gameBoard; //2D array to store the game board
+
     public Board(){
+
+
 
     }
 }
