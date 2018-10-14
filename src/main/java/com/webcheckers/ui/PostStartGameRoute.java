327d5a196ba4f63eed0618896b943258dee1eedc
Daria Chaplin <dxc4643@g.rit.edu>
2018-10-14 15:49:01 -0400

    Added some initial code to PostStartGameRoute

--- src/main/java/com/webcheckers/ui/PostStartGameRoute.java
+++ src/main/java/com/webcheckers/ui/PostStartGameRoute.java
@@ -1,14 +1,49 @@
 package com.webcheckers.ui;
 
+import com.webcheckers.application.GameCenter;
+import com.webcheckers.model.Player;
 import spark.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Logger;
 
 public class PostStartGameRoute implements Route {
 
-    public PostStartGameRoute() {
+    private static final Logger LOG = Logger.getLogger(PostStartGameRoute.class.getName());
 
+    //
+    // Attributes
+    //
+
+    private final GameCenter gameCenter;
+    private final TemplateEngine templateEngine;
+
+    /**
+     * The constructor for the {@code POST /game} route handler.
+     *
+     * @param gameCenter coordinates web app
+     * @param templateEngine template engine to use for rendering HTML page
+     * @throws NullPointerException when the {@code gameCenter} or {@code templateEngine} parameter is null
+     */
+    public PostStartGameRoute(GameCenter gameCenter, TemplateEngine templateEngine) {
+        // validation
+        Objects.requireNonNull(gameCenter, "gameCenter must not be null");
+        Objects.requireNonNull(templateEngine, "templateEngine must not be null");
+        //
+        this.gameCenter = gameCenter;
+        this.templateEngine = templateEngine;
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public Object handle(Request request, Response response) {
+        final Session session = request.session();
+        Map<String, Object> vm = new HashMap<>();
+
         return null;
     }
 }
