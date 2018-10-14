c00959ce6b97c3dc428786af2170064c4ef41483
Paula Register <per4521@g.rit.edu>
2018-10-14 17:48:56 -0400

    Implemented Iterator in Board. Added Comments

--- src/main/java/com/webcheckers/model/Message.java
+++ src/main/java/com/webcheckers/model/Message.java
@@ -6,11 +6,20 @@ public class Message {
     private Type type;
     private String text;
 
+    /**
+     *
+     * @param type
+     * @param text
+     */
     public Message(Type type, String text){
         this.type = type;
         this.text = text;
     }
 
+    /**
+     *
+     * @return
+     */
     public Type getType(){
         return this.type;
     }
