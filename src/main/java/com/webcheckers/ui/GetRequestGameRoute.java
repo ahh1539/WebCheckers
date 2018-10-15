a8ad95dc6d6ae4eb4be62e2b70dd349aa67c72fc
Daria Chaplin <dxc4643@g.rit.edu>
2018-10-15 02:44:16 -0400

    Added use of Message class for error message, added documentation

--- src/main/java/com/webcheckers/ui/GetRequestGameRoute.java
+++ src/main/java/com/webcheckers/ui/GetRequestGameRoute.java
@@ -19,10 +19,22 @@ public class GetRequestGameRoute implements Route{
         this.templateEngine = templateEngine;
     }
 
+    /**
+     *
+     * @param request
+     *      the HTTP request
+     * @param response
+     *      the HTTP response
+     * @return
+     *      the correctly rendered template for a game
+     */
     @Override
     public Object handle(Request request, Response response) {
         Map<String, Object> vm = new HashMap<>();
         vm.put("title", "Go!");
+
+        // Sets up player and opponent player based on selection
+
         final Session session = request.session();
         Player player1 =  session.attribute(PostSignInRoute.PLAYER);
         String secondPlayer = request.queryParams("opponent");
@@ -38,16 +50,20 @@ public class GetRequestGameRoute implements Route{
         // Checks whether the opponent is already playing a game
 
         if (PlayerLobby.getPlayer(player1.getName()).inGame()|| PlayerLobby.getPlayer(player2.getName()).inGame()){
-            vm.put("errorMsg", "The player you have selected is already in a game. Select another player.");
+            String msg = "The player you have selected is already in a game. Select another player.";
+            Message message = new Message(Message.Type.ERROR, msg);
+            vm.put("errorMsg", msg);
             response.redirect(WebServer.HOME_URL);
             return templateEngine.render(new ModelAndView(vm, GetHomeRoute.ROUTE_NAME));
         }
+
+        // Instantiates new game and marks both players as being participants
 
         Game game = new Game(player1, player2);
         PlayerLobby.getPlayer(player1.getName()).joinGame();
         PlayerLobby.getPlayer(player2.getName()).joinGame();
 
-
+        // Configures view model for new game
 
         gameCenter.getGameLobby().addGame(game);
         Message message = new Message(Message.Type.ERROR, "Text");
