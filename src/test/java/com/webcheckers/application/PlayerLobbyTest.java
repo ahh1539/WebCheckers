29b6932c032a8483f942696b7c102767764df7ba
ahh1539 <31779206+ahh1539@users.noreply.github.com>
2018-10-22 19:25:45 -0400

    finishing playerlobby tests

--- src/test/java/com/webcheckers/application/PlayerLobbyTest.java
+++ src/test/java/com/webcheckers/application/PlayerLobbyTest.java
@@ -27,11 +27,17 @@ public class PlayerLobbyTest extends java.lang.Object {
         lobby.addPlayer(second);
         lobby.addPlayer(third);
 
+        assertTrue(lobby instanceof PlayerLobby, "is an instance of player lobby");
+
         assertTrue(lobby.hasPlayer(first), "Player added successfully");
         assertTrue(lobby.hasPlayer(third), "Player added successfully");
 
+        assertTrue(lobby.getPlayer("Daria") == second, "Successfully found player");
+
         lobby.removePlayer(first);
         assertFalse(lobby.hasPlayer(first), "Player removed successfully");
+
+        assertTrue(lobby.getNumberOfPlayers() == 2, "Returned correct amount of players");
 
     }
 
