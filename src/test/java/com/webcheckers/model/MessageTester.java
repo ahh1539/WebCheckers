991dff88d64ed772dbf90cd14952439ff30dc53a
LilyKuhn <lvk8738@rit.edu>
2018-10-22 20:37:52 -0400

    completed testing for Message

---
+++ src/test/java/com/webcheckers/model/MessageTester.java
@@ -0,0 +1,27 @@
+package com.webcheckers.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import net.bytebuddy.build.ToStringPlugin;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class MessageTester {
+
+    @Test
+    @DisplayName("type")
+    void testType(){
+        Message m = new Message(Message.Type.ERROR, "error");
+        assertEquals(Message.Type.ERROR, m.getType());
+    }
+
+    @Test
+    @DisplayName("message string")
+    void testMessageString(){
+        Message m = new Message(Message.Type.ERROR, "error");
+        assertEquals("error", m.getText());
+    }
+
+
+}
