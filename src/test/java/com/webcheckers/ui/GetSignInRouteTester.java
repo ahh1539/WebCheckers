44bed13a380b58a6a4b36ed28689d57b1afc6de9
Daria Chaplin <dxc4643@g.rit.edu>
2018-10-22 22:52:21 -0400

    Added a unit test file for GetSignInRoute class in UI tier

---
+++ src/test/java/com/webcheckers/ui/GetSignInRouteTester.java
@@ -0,0 +1,20 @@
+package com.webcheckers.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import spark.ModelAndView;
+
+public class GetSignInRouteTester {
+
+    @Test
+    public void testSomething() {
+        // test something
+    }
+
+}
