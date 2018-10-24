72cbc5d0457ecbbb8207a0c58b5dae8622122d9d
Paula Register <per4521@g.rit.edu>
2018-10-23 21:51:55 -0400

    Fixed errors in GetSignOutRouteTest

--- src/test/java/com/webcheckers/ui/GetSignOutRouteTest.java
+++ src/test/java/com/webcheckers/ui/GetSignOutRouteTest.java
@@ -1,3 +1,88 @@
+package com.webcheckers.ui;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.webcheckers.application.GameCenter;
+import com.webcheckers.application.GameLobby;
+import com.webcheckers.application.PlayerLobby;
+import com.webcheckers.model.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import spark.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * Test class for GetSignInRoute (UI tier component)
+ * @author Daria Chaplin (dxc4643)
+ */
+@Tag("UI-tier")
+public class GetSignOutRouteTest {
+
+    /**
+     * The component-under-test (CuT).
+     */
+    private GetSignOutRoute CuT;
+
+    /**
+     * Mock objects
+     */
+    private Request request;
+    private Session session;
+    private Response response;
+    private TemplateEngine engine;
+    private PlayerLobby playerLobby;
+    private Player player;
+
+    /**
+     * Setup new mock objects for each test.
+     */
+    @BeforeEach
+    public void setup(){
+        request = mock(Request.class);
+        session = mock(Session.class);
+        when(request.session()).thenReturn(session);
+        engine = mock(TemplateEngine.class);
+
+        // set up friendly objects
+        this.playerLobby = new PlayerLobby();
+        player = new Player("player");
+        this.playerLobby.addPlayer(player);
+        GameLobby gameLobby = new GameLobby();
+        GameCenter gameCenter = new GameCenter(playerLobby, gameLobby);
+
+        // component-under-test
+        CuT = new GetSignOutRoute(engine, gameCenter);
+    }
+
+    @Test
+    public void signOutTest(){
+        final TemplateEngineTester testHelper = new TemplateEngineTester();
+        when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+
+        CuT.handle(request, response);
+
+
+        // Analyze the results:
+        //   * model is a non-null Map
+        testHelper.assertViewModelExists();
+        testHelper.assertViewModelIsaMap();
+        //   * model contains all necessary View-Model data
+
+        testHelper.assertViewModelAttribute(GetHomeRoute.TITLE_ATTR, GetSignOutRoute.TITLE);
+        testHelper.assertViewModelAttribute(GetHomeRoute.PLAYER, null);
+        testHelper.assertViewModelAttribute(GetHomeRoute.PLAYER_LIST, null);
+        //   * test view name
+        testHelper.assertViewName(GetHomeRoute.ROUTE_NAME);
+        testHelper.assertViewModelAttribute(GetHomeRoute.NUM_PLAYERS, playerLobby.getNumberOfPlayers());
+    }
+
+}
+
+
 /*
 package com.webcheckers.ui;
 import com.webcheckers.application.GameCenter;
