cb201935b137ec9266e3f90dad2bcfc794119b44
Daria Chaplin <dxc4643@g.rit.edu>
2018-10-29 19:10:48 -0400

    Refactored PieceTest to work with updates Piece class that only includes single pieces

--- src/test/java/com/webcheckers/model/PieceTest.java
+++ src/test/java/com/webcheckers/model/PieceTest.java
@@ -16,82 +16,34 @@ public class PieceTest {
     private static final Piece.Color redColor = Piece.Color.RED;
     private static final Piece.Color whiteColor = Piece.Color.WHITE;
 
-    private static final Piece.Type singleType = Piece.Type.SINGLE;
-    private static final Piece.Type kingType = Piece.Type.KING;
-
     private static final int index = 0;
 
-
     /**
-     * Test that the constructor works with two arguments
+     * Testing constructor for single red piece
      */
     @Test
-    @DisplayName("Piece Two Argument Constructor")
-    public void ctor_two_args() {
-        final Piece CuT = new Piece(redColor, singleType);
-        assertEquals(redColor, CuT.getColor());
-        assertEquals(singleType, CuT.getType());
-    }
-
-    /**
-     * Test that the constructor works with three arguments
-     */
-    @Test
-    @DisplayName("Piece Three Argument Constructor")
-    public void ctor_three_args() {
-        final Piece CuT = new Piece(redColor, singleType, index);
-        assertEquals(redColor, CuT.getColor());
-        assertEquals(singleType, CuT.getType());
-        assertEquals(index, CuT.getIndex());
-    }
-
-    /**
-     * Testing for single red piece
-     */
-    @Test
-    public void test_red_single(){
-        final Piece CuT = new Piece(redColor, singleType);
+    public void test_red_piece(){
+        final Piece CuT = new Piece(redColor, index);
         assertEquals(Piece.Color.RED, CuT.getColor());
-        assertEquals(Piece.Type.SINGLE, CuT.getType());
     }
 
     /**
-     * Testing for single white piece
+     * Testing constructor for single white piece
      */
     @Test
     public void test_white_single(){
-        final Piece CuT = new Piece(whiteColor, singleType);
+        final Piece CuT = new Piece(whiteColor, index);
         assertEquals(Piece.Color.WHITE, CuT.getColor());
-        assertEquals(Piece.Type.SINGLE, CuT.getType());
     }
 
-    /**
-     * Testing for king red piece
-     */
-    @Test
-    public void test_red_king(){
-        final Piece CuT = new Piece(redColor, kingType);
-        assertEquals(Piece.Color.RED,CuT.getColor());
-        assertEquals(Piece.Type.KING,CuT.getType());
-    }
-
-    /**
-     * Testing for king white piece
-     */
-    @Test
-    public void test_white_king(){
-        final Piece CuT = new Piece(whiteColor, kingType);
-        assertEquals(Piece.Color.WHITE,CuT.getColor());
-        assertEquals(Piece.Type.KING,CuT.getType());
-    }
     /**
      * Testing the .equals method of Piece to check it outputs True
      */
     @Test
     public void test_equals_true(){
-        final Piece CuT = new Piece(redColor,singleType);
-        final Piece CuT2 = new Piece(redColor,singleType);
-        assertEquals(true, CuT.equals(CuT2));
+        final Piece CuT = new Piece(redColor, index);
+        final Piece CuT2 = new Piece(redColor, index);
+        assertTrue(CuT.equals(CuT2));
     }
 
     /**
@@ -99,20 +51,8 @@ public class PieceTest {
      */
     @Test
     public void test_equals_false(){
-        final Piece CuT = new Piece(redColor,singleType);
-        final Piece CuT2 = new Piece(whiteColor, kingType);
-        assertEquals(false, CuT.equals(CuT2));
-    }
-
-
-    /**
-     * Test crownPiece
-     */
-    @Test
-    public void test_crown_piece(){
-        final Piece CuT = new Piece(redColor, singleType);
-        CuT.crownPiece();
-        assertEquals(Piece.Color.RED, CuT.getColor());
-        assertEquals(Piece.Type.KING, CuT.getType());
+        final Piece CuT = new Piece(redColor, 0);
+        final Piece CuT2 = new Piece(whiteColor, 0);
+        assertFalse(CuT.equals(CuT2));
     }
 }
