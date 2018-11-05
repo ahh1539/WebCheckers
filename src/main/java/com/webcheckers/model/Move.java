18ed26ae44af7a560b050a52683d77cd11f78fea
Paula Register <per4521@g.rit.edu>
2018-11-05 09:20:12 -0500

    Implemented logic for isValidMove

--- src/main/java/com/webcheckers/model/Move.java
+++ src/main/java/com/webcheckers/model/Move.java
@@ -26,12 +26,70 @@ public class Move implements Serializable {
     }
 
     public BoardView getBoard() {
-        return board;
-        //return new BoardView(this.board);
+        return new BoardView(this.board);
     }
 
     public String toString(){
         return "start: ("+ start.getRow() +", "+start.getCell()+")"+
                 " end: ("+ end.getRow() +", "+end.getCell()+")";
+    }
+
+    public BoardView makeMove(){
+        BoardView copyBoard = new BoardView(this.board);
+        //Piece piece = copyBoard.getRow(this.start.getRow())
+        return copyBoard;
+    }
+
+    /**
+     * Checks if the current move is valid.
+     * @return
+     */
+    public boolean isValid(){
+        boolean valid = false;
+        // Get the starting space and end space
+        Row startRow = this.board.getRow(this.start.getRow());
+        Space startSpace = startRow.getSpace(this.start.getCell());
+        Row endRow = this.board.getRow(this.end.getRow());
+        Space endSpace = endRow.getSpace(this.end.getCell());
+
+        Piece movingPiece = startSpace.getPiece();
+        // Check if the piece is single
+        if(movingPiece.getType() == Piece.Type.SINGLE){
+            // If it is Red
+            if(startSpace.getPiece().getColor() == Piece.Color.RED){
+                // check if regular move and if destination is valid
+                int startRowIndex = this.start.getRow();
+                int endRowIndex = this.end.getRow();
+                int startCell = this.start.getCell();
+                int endCell = this.end.getCell();
+
+                boolean isJumpRow = (startRowIndex - 2) == endRowIndex;
+                boolean isJumpCell = (startCell-2 == endCell);
+                boolean correctRows = (startRowIndex-1 == endRowIndex);
+                boolean correctCell = (startCell+1 == endCell);
+
+                if((correctCell && correctRows) || (startCell-1 == endCell)){
+                    if(endSpace.isValid()){
+                        valid = true;
+                    }
+                }
+                else if ((isJumpRow && isJumpCell) || (startCell+2 == endCell)){
+                    int targetRow = (startRowIndex + endRowIndex) / 2;
+                    int targetCell = (startCell + endCell) / 2;
+                    Space targetSpace = this.board.getRow(targetRow).getSpace(targetCell);
+                    if(endSpace.isValid() && !targetSpace.isValid()){
+                        if(targetSpace.getPiece().getColor() != movingPiece.getColor()){
+                            valid = true;
+                        }
+                    }
+                }
+            }
+            // If it is White
+        }
+        // If the piece is a King
+        else {
+            // Check if it is regular jump
+        }
+        return valid;
     }
 }
