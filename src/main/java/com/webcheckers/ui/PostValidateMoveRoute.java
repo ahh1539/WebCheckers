f7da9e01d6a2a1acabbd875e5a6ab4dfc830c145
Daria Chaplin <dxc4643@g.rit.edu>
2018-11-06 19:09:01 -0500

    Cleaned up and further documented PostValidateMoveRoute

--- src/main/java/com/webcheckers/ui/PostValidateMoveRoute.java
+++ src/main/java/com/webcheckers/ui/PostValidateMoveRoute.java
@@ -3,47 +3,47 @@ package com.webcheckers.ui;
 import com.google.gson.Gson;
 import com.webcheckers.application.GameCenter;
 import com.webcheckers.model.*;
+import freemarker.template.Template;
 import spark.*;
 
 import java.util.Objects;
 import java.util.logging.Logger;
 
 public class PostValidateMoveRoute implements Route {
-    private static final Logger LOG = Logger.getLogger(GetSignInRoute.class.getName());
 
+    // Attributes
+    private static final Logger LOG = Logger.getLogger(GetSignInRoute.class.getName());
     private final TemplateEngine templateEngine;
     private final GameCenter gameCenter;
-
     private Gson gson;
 
     /**
      * Create the Spark Route (UI controller) for the
-     * {@code GET /} HTTP request.
+     * {@code POST /validateMove} HTTP request.
      *
      * @param templateEngine
      *   the HTML template rendering engine
      */
     public PostValidateMoveRoute(final TemplateEngine templateEngine, final GameCenter gameCenter) {
-        // validation
+        // Validation
         Objects.requireNonNull(templateEngine, "templateEngine must not be null");
         Objects.requireNonNull(gameCenter, "gameCenter must not be null");
-        //
+        // Configuration
         this.templateEngine = templateEngine;
         this.gameCenter = gameCenter;
-        //
         LOG.config("PostValidateMoveRoute is initialized.");
     }
 
     @Override
     public Object handle(Request request, Response response) {
-
-        // grab game from session
+        // Grab game from session
         final Session session = request.session();
 
-        // get current player and game to compare active color with
+        // Get current player and game to compare active color with
         Player player = session.attribute(PostSignInRoute.PLAYER);
         Game game = gameCenter.getGameLobby().getGame(player);
         BoardView board;
+
         if(player.getColor() == Color.RED){
             board = game.getRedBoard();
         }
@@ -51,18 +51,17 @@ public class PostValidateMoveRoute implements Route {
             board = game.getWhiteBoard();
         }
 
-        // grab move from request
+        // Grab move from request
         final Gson gson = new Gson();
         final String json = request.body();
         final Move move = gson.fromJson(json, Move.class);
 
-        // check if it's valid, and format response message to JSON
+        // Check if it's valid, and format response message to JSON
         Message message = move.isValidMessage(board);
         if (!(message.getType() == Message.Type.ERROR)){
             game.addMove(move);
         }
-        String rjson = gson.toJson(message);
 
-        return rjson;
+        return gson.toJson(message);
     }
 }
